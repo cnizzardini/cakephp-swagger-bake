4c0ebcb923450527277b070d7420aea2a9a82ee1
chris cnizzardini <cnizzardini@gmail.com>
2020-05-06 21:53:49 -0400

    Added local redoc.standalone.js version at 2.0.0-rc.28

--- templates/Swagger/redoc.php
+++ templates/Swagger/redoc.php
@@ -1,2 +1,3 @@
 <redoc spec-url='<?php echo $url ?>'></redoc>
-<script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"> </script>
+<!--script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"> </script>-->
+<script src="/swagger_bake/redoc.standalone.js"> </script>
