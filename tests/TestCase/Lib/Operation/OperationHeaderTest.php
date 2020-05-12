25fb95a92d14123a3a936778ba8a2093ee0614a3
chris cnizzardini <cnizzardini@gmail.com>
2020-05-11 23:27:20 -0400

    removed imports

--- tests/TestCase/Lib/Operation/OperationHeaderTest.php
+++ tests/TestCase/Lib/Operation/OperationHeaderTest.php
@@ -2,19 +2,10 @@
 
 namespace SwaggerBake\Test\TestCase\Lib\Operation;
 
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 use Cake\TestSuite\TestCase;
 use SwaggerBake\Lib\Annotation\SwagHeader;
-use SwaggerBake\Lib\AnnotationLoader;
-use SwaggerBake\Lib\CakeModel;
-use SwaggerBake\Lib\CakeRoute;
-use SwaggerBake\Lib\Configuration;
-use SwaggerBake\Lib\Model\ExpressiveRoute;
 use SwaggerBake\Lib\OpenApi\Operation;
-use SwaggerBake\Lib\Operation\OperationFromRouteFactory;
 use SwaggerBake\Lib\Operation\OperationHeader;
-use SwaggerBake\Lib\Swagger;
 
 class OperationHeaderTest extends TestCase
 {
