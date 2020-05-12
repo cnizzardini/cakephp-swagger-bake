18c2bd9204d367132d2b2f5c77a0400af3c4b134
chris cnizzardini <cnizzardini@gmail.com>
2020-05-12 17:28:24 -0400

    OperationSecurity

---
+++ src/Lib/Operation/OperationSecurity.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SwaggerBake\Lib\Operation;
+
+use SwaggerBake\Lib\Annotation\SwagSecurity;
+use SwaggerBake\Lib\OpenApi\Operation;
+use SwaggerBake\Lib\OpenApi\PathSecurity;
+
+class OperationSecurity
+{
+    /**
+     * @param Operation $operation
+     * @param array $annotations
+     * @return Operation
+     */
+    public function getOperationWithSecurity(Operation $operation, array $annotations) : Operation
+    {
+        $swagSecurities = array_filter($annotations, function ($annotation) {
+            return $annotation instanceof SwagSecurity;
+        });
+
+        foreach ($swagSecurities as $annotation) {
+            $operation->pushSecurity(
+                (new PathSecurity())
+                    ->setName($annotation->name)
+                    ->setScopes($annotation->scopes)
+            );
+        }
+
+        return $operation;
+    }
+}
