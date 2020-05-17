807933b4a1a2aeb86ec0c6c0b6b22d21d4262163
chris cnizzardini <cnizzardini@gmail.com>
2020-05-17 10:28:26 -0400

    NamespaceUtility

--- src/Lib/Utility/NamespaceUtility.php
+++ src/Lib/Utility/NamespaceUtility.php
@@ -2,6 +2,7 @@
 
 namespace SwaggerBake\Lib\Utility;
 
+use Cake\ORM\Table;
 use SwaggerBake\Lib\Configuration;
 use SwaggerBake\Lib\Exception\SwaggerBakeRunTimeException;
 
@@ -32,6 +33,60 @@ class NamespaceUtility
             $entity = $namespace . 'Controller\\' . $className;
             if (class_exists($entity, true)) {
                 return $entity;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets a FQNS of an Entity
+     *
+     * @param string $className
+     * @param Configuration $config
+     * @return string|null
+     */
+    public static function getEntityFullyQualifiedNameSpace(string $className, Configuration $config) : ?string
+    {
+        $namespaces = $config->getNamespaces();
+
+        if (!isset($namespaces['entities']) || !is_array($namespaces['entities'])) {
+            throw new SwaggerBakeRunTimeException(
+                'Invalid configuration, missing SwaggerBake.namespaces.entities'
+            );
+        }
+
+        foreach ($namespaces['entities'] as $namespace) {
+            $entity = $namespace . 'Model\Entity\\' . $className;
+            if (class_exists($entity, true)) {
+                return $entity;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets a FQNS of a Table
+     *
+     * @param string $className
+     * @param Configuration $config
+     * @return string|null
+     */
+    public static function getTableFullyQualifiedNameSpace(string $className, Configuration $config) : ?string
+    {
+        $namespaces = $config->getNamespaces();
+
+        if (!isset($namespaces['tables']) || !is_array($namespaces['tables'])) {
+            throw new SwaggerBakeRunTimeException(
+                'Invalid configuration, missing SwaggerBake.namespaces.tables'
+            );
+        }
+
+        foreach ($namespaces['tables'] as $namespace) {
+            $table = $namespace . 'Model\Table\\' . $className;
+            if (class_exists($table, true)) {
+                return $table;
             }
         }
 
