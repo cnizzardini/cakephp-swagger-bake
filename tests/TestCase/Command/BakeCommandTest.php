acfbae122b602a68f101f0d5c36464d1e64c6950
chris cnizzardini <cnizzardini@gmail.com>
2020-06-18 17:54:08 -0400

    #50 Unit tests for BakeCommand `swagger bake`

--- tests/TestCase/Command/BakeCommandTest.php
+++ tests/TestCase/Command/BakeCommandTest.php
@@ -2,12 +2,29 @@
 
 namespace SwaggerBake\Test\TestCase\Command;
 
+use Cake\TestSuite\ConsoleIntegrationTestTrait;
 use Cake\TestSuite\TestCase;
 
 class BakeCommandTest extends TestCase
 {
+    use ConsoleIntegrationTestTrait;
+
+    public $fixtures = [
+        'plugin.SwaggerBake.Departments'
+    ];
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->setAppNamespace('SwaggerBakeTest\App');
+        $this->useCommandRunner();
+    }
+
     public function testExecute()
     {
-        $this->markTestIncomplete();
+        unlink(WWW_ROOT . DS . 'swagger.json');
+        $this->exec('swagger bake');
+        $this->assertOutputContains('Running...');
+        $this->assertOutputContains('Swagger File Created:');
     }
 }
