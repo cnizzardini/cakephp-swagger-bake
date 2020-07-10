4d663aec50044b305019df59b3b271d0d6352ec4
chris cnizzardini <cnizzardini@gmail.com>
2020-07-10 00:15:18 -0400

    Full unit test coverage for NamespaceUtility

---
+++ tests/TestCase/Lib/Utility/NamespaceUtilityTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace SwaggerBake\Test\TestCase\Lib\Utility;
+
+use Cake\TestSuite\TestCase;
+use SwaggerBake\Lib\Configuration;
+use SwaggerBake\Lib\Exception\SwaggerBakeRunTimeException;
+use SwaggerBake\Lib\Utility\NamespaceUtility;
+
+class NamespaceUtilityTest extends TestCase
+{
+    public function testGetControllerFullQualifiedNameSpace()
+    {
+        $fqns = NamespaceUtility::getControllerFullQualifiedNameSpace(
+            'DepartmentsController',
+            new Configuration([
+                'namespaces' => [
+                    'controllers' => ['\SwaggerBakeTest\App\\']
+                ]
+            ])
+        );
+        $this->assertEquals('\SwaggerBakeTest\App\Controller\DepartmentsController', $fqns);
+    }
+
+    public function testGetControllerFullQualifiedNameSpaceException()
+    {
+        $this->expectException(SwaggerBakeRunTimeException::class);
+        NamespaceUtility::getControllerFullQualifiedNameSpace('DepartmentsController', new Configuration());
+    }
+
+    public function testGetControllerFullQualifiedNameSpaceNull()
+    {
+        $fqns = NamespaceUtility::getControllerFullQualifiedNameSpace(
+            'NopeController',
+            new Configuration([
+                'namespaces' => [
+                    'tables' => ['\SwaggerBakeTest\App\\'],
+                ]
+            ])
+        );
+        $this->assertNull($fqns);
+    }
+
+    public function testGetEntityFullyQualifiedNameSpace()
+    {
+        $fqns = NamespaceUtility::getEntityFullyQualifiedNameSpace(
+            'Department',
+            new Configuration([
+                'namespaces' => [
+                    'entities' => ['\SwaggerBakeTest\App\\'],
+                ]
+            ])
+        );
+        $this->assertEquals('\SwaggerBakeTest\App\Model\Entity\Department', $fqns);
+    }
+
+    public function testGetEntityFullyQualifiedNameSpaceException()
+    {
+        $this->expectException(SwaggerBakeRunTimeException::class);
+        NamespaceUtility::getEntityFullyQualifiedNameSpace('Department', new Configuration());
+    }
+
+    public function testGetEntityFullyQualifiedNameSpaceNull()
+    {
+        $fqns = NamespaceUtility::getEntityFullyQualifiedNameSpace(
+            'Nope',
+            new Configuration([
+                'namespaces' => [
+                    'tables' => ['\SwaggerBakeTest\App\\'],
+                ]
+            ])
+        );
+        $this->assertNull($fqns);
+    }
+
+    public function testGetTableFullyQualifiedNameSpace()
+    {
+        $fqns = NamespaceUtility::getTableFullyQualifiedNameSpace(
+            'DepartmentsTable',
+            new Configuration([
+                'namespaces' => [
+                    'tables' => ['\SwaggerBakeTest\App\\'],
+                ]
+            ])
+        );
+        $this->assertEquals('\SwaggerBakeTest\App\Model\Table\DepartmentsTable', $fqns);
+    }
+
+    public function testGetTableFullyQualifiedNameSpaceException()
+    {
+        $this->expectException(SwaggerBakeRunTimeException::class);
+        NamespaceUtility::getTableFullyQualifiedNameSpace('DepartmentsTable', new Configuration());
+    }
+
+    public function testGetTableFullyQualifiedNameSpaceNull()
+    {
+        $fqns = NamespaceUtility::getTableFullyQualifiedNameSpace(
+            'NopeTable',
+            new Configuration([
+                'namespaces' => [
+                    'tables' => ['\SwaggerBakeTest\App\\'],
+                ]
+            ])
+        );
+        $this->assertNull($fqns);
+    }
+}
