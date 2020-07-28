50ce4b3a25a96d9a6a347b6499605961681bbd84
chris cnizzardini <cnizzardini@gmail.com>
2020-07-27 21:53:11 -0400

    added unit test

--- tests/TestCase/Lib/Operation/OperationFromRouteFactoryTest.php
+++ tests/TestCase/Lib/Operation/OperationFromRouteFactoryTest.php
@@ -27,7 +27,7 @@ class OperationFromRouteFactoryTest extends TestCase
         $router::scope('/api', function (RouteBuilder $builder) {
             $builder->setExtensions(['json']);
             $builder->resources('Employees', [
-                'only' => 'index'
+                'only' => ['index','edit']
             ]);
         });
         $this->router = $router;
@@ -64,5 +64,24 @@ class OperationFromRouteFactoryTest extends TestCase
         $this->assertEquals('GET', $operation->getHttpMethod());
         $this->assertEquals('employees:index:get', $operation->getOperationId());
         $this->assertEquals('CustomTag', $operation->getTags()[1]);
+    }
+
+    /**
+     * Tests `@SwagOperation(showPut=true)`
+     */
+    public function testShowPut()
+    {
+        $config = new Configuration($this->config, SWAGGER_BAKE_TEST_APP);
+        $swagger = (new SwaggerFactory($config))->create();
+        $cakeRoute = new CakeRoute($this->router, $config);
+
+        $routes = $cakeRoute->getRoutes();
+        $route = reset($routes);
+
+        $operation = (new OperationFromRouteFactory($swagger))->create($route, 'PUT', null);
+
+        $this->assertInstanceOf(Operation::class, $operation);
+        $this->assertEquals('PUT', $operation->getHttpMethod());
+        $this->assertEquals('employees:index:put', $operation->getOperationId());
     }
 }
