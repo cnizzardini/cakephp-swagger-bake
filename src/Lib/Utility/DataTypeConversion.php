06dc24e280e566331f6a8b0a8680ceaafe553ea1
chris cnizzardini <cnizzardini@gmail.com>
2020-11-14 09:18:07 -0500

    comments

--- src/Lib/Utility/DataTypeConversion.php
+++ src/Lib/Utility/DataTypeConversion.php
@@ -11,7 +11,7 @@ namespace SwaggerBake\Lib\Utility;
 class DataTypeConversion
 {
     /**
-     * Returns the OpenApi data type from the CakePHP data type
+     * Returns an OpenApi data type from the CakePHP data type, will default to returning argument if no match found
      *
      * @param string $cakeType Data type
      * @return string
@@ -19,6 +19,7 @@ class DataTypeConversion
     public static function toType(string $cakeType): string
     {
         $typeMap = [
+            // openapi type => ['cake','types']
             'integer' => ['int','integer','tinyinteger','smallinteger','biginteger','mediuminteger'],
             'number' => ['decimal','float'],
             'string' => ['uuid','text','varchar','char','date','time','datetime'],
@@ -35,7 +36,7 @@ class DataTypeConversion
     }
 
     /**
-     * Returns a data format from CakePHP data type
+     * Returns an OpenAPI data format from CakePHP data type
      *
      * @param string $cakeType Data type
      * @return string
@@ -43,6 +44,7 @@ class DataTypeConversion
     public static function toFormat(string $cakeType): string
     {
         $typeMap = [
+            // openapi format => ['cake','types']
             'int64' => ['int','integer','biginteger'],
             'int32' => ['tinyinteger','smallinteger','mediuminteger'],
             'float' => ['decimal','float'],
