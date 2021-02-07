aae9fcd85db141b1d211497fbeb24d92b93ee143
chris cnizzardini <cnizzardini@gmail.com>
2021-02-07 11:52:55 -0500

    Require using FQN for exception `@throws` to resolve bug
    Resolves #246

--- tests/TestCase/Lib/Operation/ExceptionHandlerTest.php
+++ tests/TestCase/Lib/Operation/ExceptionHandlerTest.php
@@ -8,27 +8,29 @@ use SwaggerBake\Lib\Operation\ExceptionHandler;
 
 class ExceptionHandlerTest extends TestCase
 {
-    public function testErrorCodes()
+    public function testErrorCodes(): void
     {
         $exceptions = [
-            400 => 'BadRequestException',
-            401 => 'UnauthorizedException',
-            403 => 'ForbiddenException',
-            404 => 'RecordNotFoundException',
-            405 => 'MethodNotAllowedException',
-            500 => 'Exception',
+            '400' => '\Cake\Http\Exception\BadRequestException',
+            '401' => '\Cake\Http\Exception\UnauthorizedException',
+            '403' => '\Cake\Http\Exception\ForbiddenException',
+            '404' => '\Cake\Datasource\Exception\RecordNotFoundException',
+            '405' => '\Cake\Http\Exception\MethodNotAllowedException',
+            '500' => '\Exception'
         ];
 
         $factory = DocBlockFactory::createInstance();
         foreach ($exceptions as $code => $exception) {
+            /** @var \phpDocumentor\Reflection\DocBlock\Tags\Throws $throws */
             $throws = $factory->create("/** @throws $exception */ */")->getTagsByName('throws')[0];
             $this->assertEquals($code, (new ExceptionHandler($throws))->getCode());
         }
     }
 
-    public function testMessage()
+    public function testMessage(): void
     {
         $factory = DocBlockFactory::createInstance();
+        /** @var \phpDocumentor\Reflection\DocBlock\Tags\Throws $throws */
         $throws = $factory->create("/** @throws Exception description */")->getTagsByName('throws')[0];
         $this->assertEquals('description', (new ExceptionHandler($throws))->getMessage());
     }
