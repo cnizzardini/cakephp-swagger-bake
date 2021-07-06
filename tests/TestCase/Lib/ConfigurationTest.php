f5cea57640d1b2853f636fb07018d578faece575
chris cnizzardini <cnizzardini@gmail.com>
2021-07-06 17:00:26 -0400

    increase code coverage

---
+++ tests/TestCase/Lib/ConfigurationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SwaggerBake\Test\TestCase\Lib;
+
+use Cake\TestSuite\TestCase;
+use LogicException;
+use SwaggerBake\Lib\Configuration;
+
+class ConfigurationTest extends TestCase
+{
+    public function test_get_variable_does_not_exist_logic_exception(): void
+    {
+        $this->expectException(LogicException::class);
+        (new Configuration())->get('nope');
+    }
+
+    public function test_set_variable_does_not_exist_logic_exception(): void
+    {
+        $this->expectException(LogicException::class);
+        (new Configuration())->set('nope', 'value');
+    }
+
+    public function test_get_hot_reload(): void
+    {
+        $this->assertFalse(@(new Configuration())->getHotReload());
+    }
+
+    public function test_get_doc_type(): void
+    {
+        $this->assertEquals('swagger', (new Configuration())->getDocType());
+    }
+}
