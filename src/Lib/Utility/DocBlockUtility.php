e838a0676723fc9977cc5beb1c53f8c9df4c32b0
chris cnizzardini <cnizzardini@gmail.com>
2021-10-22 17:08:33 -0400

    remove unused methods

--- src/Lib/Utility/DocBlockUtility.php
+++ src/Lib/Utility/DocBlockUtility.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 namespace SwaggerBake\Lib\Utility;
 
 use phpDocumentor\Reflection\DocBlock;
-use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use phpDocumentor\Reflection\DocBlockFactory;
-use phpDocumentor\Reflection\Types\Boolean;
-use phpDocumentor\Reflection\Types\Integer;
-use phpDocumentor\Reflection\Types\String_;
-use ReflectionClass;
 use ReflectionMethod;
-use ReflectionProperty;
 
 /**
  * Class DocBlockUtility
@@ -44,66 +38,5 @@ class DocBlockUtility
         $docFactory = DocBlockFactory::createInstance();
 
         return $docFactory->create($comment);
-    }
-
-    /**
-     * Returns key-value array of property name and ReflectedProperty
-     *
-     * @param object $instance Object
-     * @return \ReflectionProperty[]
-     */
-    public static function getProperties(object $instance): ?array
-    {
-        try {
-            $reflectionClass = new ReflectionClass(get_class($instance));
-            $reflectedProperties = $reflectionClass->getProperties();
-        } catch (\ReflectionException $e) {
-            return null;
-        }
-
-        if (empty($reflectedProperties)) {
-            return null;
-        }
-
-        $return = [];
-        foreach ($reflectedProperties as $property) {
-            $return[$property->name] = $property;
-        }
-
-        return $return ?? null;
-    }
-
-    /**
-     * @param \ReflectionProperty $property ReflectionProperty
-     * @return \phpDocumentor\Reflection\DocBlock
-     */
-    public static function getPropertyDocBlock(ReflectionProperty $property): DocBlock
-    {
-        print_r($property);
-        $comment = $property->getDocComment();
-        $docFactory = DocBlockFactory::createInstance();
-
-        return $docFactory->create($comment);
-    }
-
-    /**
-     * Returns string representation of Var_ data type. Can be either string, integer, boolean, or null if unknown
-     *
-     * @param \phpDocumentor\Reflection\DocBlock\Tags\Var_ $var DocBlock\Tags\Var_
-     * @return string|null
-     */
-    public static function getDocBlockConvertedVar(Var_ $var): ?string
-    {
-        if ($var->getType() instanceof String_) {
-            return 'string';
-        }
-        if ($var->getType() instanceof Integer) {
-            return 'integer';
-        }
-        if ($var->getType() instanceof Boolean) {
-            return 'boolean';
-        }
-
-        return null;
     }
 }
