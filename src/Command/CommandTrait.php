f91b8a40327d6c162f3742dcac846138c5e33922
chris cnizzardini <cnizzardini@gmail.com>
2021-11-24 15:21:47 -0500

    Fixes unit test

--- src/Command/CommandTrait.php
+++ src/Command/CommandTrait.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace SwaggerBake\Command;
 
 use Cake\Core\Configure;
-use Cake\Core\Exception\CakeException;
+use Exception;
 use SwaggerBake\Lib\AnnotationLoader;
 use SwaggerBake\Lib\Exception\SwaggerBakeRunTimeException;
 use SwaggerBake\Lib\ExtensionLoader;
@@ -25,9 +25,11 @@ trait CommandTrait
             Configure::delete('SwaggerBake');
         }
 
+        // Catch a generic exception here to support older versions of CakePHP which throw
+        // \Cake\Core\Exception\Exception instead of \Cake\Core\Exception\CakeException in newer versions
         try {
             Configure::load($config, 'default');
-        } catch (CakeException $e) {
+        } catch (Exception $e) {
             throw new SwaggerBakeRunTimeException(
                 "SwaggerBake config file `$config` is missing or " . get_class($e) . ' ' . $e->getMessage()
             );
