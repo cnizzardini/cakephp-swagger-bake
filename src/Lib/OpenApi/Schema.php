27d8760b72132103dc1e0d6542d07abac6fcb615
Jamison Bryant <jamison@bryant.ai>
2023-01-03 21:42:13 -0500

    Add phpstan ignore comment

--- src/Lib/OpenApi/Schema.php
+++ src/Lib/OpenApi/Schema.php
@@ -14,6 +14,7 @@ use SwaggerBake\Lib\Utility\ArrayUtility;
  * @see https://swagger.io/docs/specification/data-models/
  * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
  */
+/** @phpstan-ignore-next-line */
 #[\AllowDynamicProperties]
 class Schema implements JsonSerializable, SchemaInterface
 {
