210db40460c2fca4c368b019376e1cdc85a101ca
Jamison Bryant <jbryant@ticketsauce.com>
2025-10-17 09:49:22 -0400

    Pass config into Association class

--- src/Lib/Operation/OperationResponse.php
+++ src/Lib/Operation/OperationResponse.php
@@ -281,7 +281,7 @@ class OperationResponse
     private function addAssociatedSchema(Response $response, string $mimeType, OpenApiResponse $openApiResponse): bool
     {
         if (is_array($openApiResponse->associations)) {
-            $assocSchema = (new OperationResponseAssociation($this->swagger, $this->route, $this->schema))
+            $assocSchema = (new OperationResponseAssociation($this->swagger, $this->config, $this->route, $this->schema))
                 ->build($openApiResponse);
             $schema = $this->getMimeTypeSchema(
                 $mimeType,
